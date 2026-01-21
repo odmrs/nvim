9ecbffbd8ab9f4b32dde7d453cfc62ca11e5f1f4
odmrs <omarcosviniciusdev@gmail.com>
2026-01-21 07:17:53 -0300

    feat: install CPP package

--- lua/community.lua
+++ lua/community.lua
@@ -9,4 +9,5 @@ return {
   { import = "astrocommunity.search.grug-far-nvim"},
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.pack.cpp" },
 }
--- lua/plugins/astrocommunity.lua
+++ lua/plugins/astrocommunity.lua
@@ -4,4 +4,5 @@ return {
   { import = "astrocommunity.search.grug-far-nvim" },
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.pack.cpp" },
 }
